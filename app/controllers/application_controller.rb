3a33c461d47325e0aa46e79b4e79fc37ba95feb1
Kasey Arnold <kaseyarnold15@gmail.com>
2020-05-07 04:37:22 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,43 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-  end
+  get '/articles/new' do 
+      erb :new
+    end
+  
+    post '/articles' do  
+      @article = Article.create(params)
+      redirect to '/articles'
+    end 
+  
+    get '/articles' do 
+      @articles = Article.all
+      erb :index
+    end
+  
+    get '/articles/:id' do  
+      @article = Article.find_by_id(params[:id])
+      erb :show
+    end
+  
+    get '/articles/:id/edit' do 
+      @article = Article.find_by_id(params[:id])
+      erb :edit
+    end
+  
+    patch '/articles/:id' do  
+      @article = Article.find_by_id(params[:id])
+      @article.name = params[:name]
+      @article.content = params[:content]
+      @article.save
+      erb :show
+    end
+  
+  
+    delete '/articles/:id/delete' do 
+      @article = Article.find_by_id(params[:id])
+      @articles.delete
+      redirect to '/articles'
+    end
+  
 end
